8cb6d2e7523caf990403ebbb7262f4a1dc61e2ed
JeeveshJ7 <jeeveshj@LT-MBP-248.local>
2024-05-17 13:20:05 +0530

    SmartUI SDK Wdio Local and Cloud

---
+++ test/specs/cloud.e2e.js
@@ -0,0 +1,11 @@
+const { expect, browser, $ } = require('@wdio/globals')
+const { smartuiSnapshot } = require('@lambdatest/wdio-driver');
+
+describe('My first visual test', () => {
+    it('should use appropriate project token', async () => {
+        await browser.url(`https://webdriver.io`)
+        await smartuiSnapshot(browser, "SS-1");
+
+    })
+})
+
---
+++ test/specs/local.e2e.js
@@ -0,0 +1,23 @@
+const { smartuiSnapshot } = require('@lambdatest/wdio-driver');
+const { remote } = require('webdriverio');
+
+(async function example() {
+    const browser = await remote({
+        capabilities: {
+            browserName: 'chrome',
+            'goog:chromeOptions': {
+                args: process.env.CI ? ['headless', 'disable-gpu'] : []
+            }
+        }
+    });
+
+    try {
+        await browser.url('https://webdriver.io');
+        await smartuiSnapshot(browser, "SS-1");
+        
+    } catch (error) {
+        console.error('Error occurred:', error);
+    } finally {
+        await browser.deleteSession();
+    }
+})();
